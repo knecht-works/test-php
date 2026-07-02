fc264169829f320307b63fd3b9406bdeeb6c5127
Knecht <knecht@users.noreply.github.com>
2026-07-02 12:03:00 +0200

    Add multisite hostnames to e2e fixture

--- public/index.php
+++ public/index.php
@@ -1,2 +1,4 @@
 <?php
-echo "knecht-e2e: hello from " . ($_SERVER["HTTP_HOST"] ?? "unknown") . " at " . date("c");
+echo "knecht-e2e: hello from " . ($_SERVER["HTTP_HOST"] ?? "unknown") . "\n";
+echo '<a href="https://beta.test-php.ddev.site/page">beta link</a>' . "\n";
+echo '<a href="https://test-php.ddev.site/home">primary link</a>' . "\n";
